e57a737a74df9fddbe709e89d34c86a519799220
Folashade Bello <folashadebello.b@gmail.com>
2025-05-20 16:22:26 +0100

    initial commit

---
+++ 1-multi_languages.js
@@ -0,0 +1,6 @@
+const cLanaguage = "C is fun"
+const pythonLanguage = "Python is cool"
+const jsLanguage = "JavaScript is amazing"
+console.log(cLanaguage)
+console.log(pythonLanguage)
+console.log(jsLanguage)
