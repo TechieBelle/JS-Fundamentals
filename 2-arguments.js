9428f5c104fa1b196a62659073a3fc4e6f0d2d90
Folashade Bello <folashadebello.b@gmail.com>
2025-05-21 22:00:45 +0100

    updated

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,3 +1,17 @@
+// function showArguments(...args) {
+//   if (args.length < 1) {
+//     console.log("No argument");
+//   } else if (args.length == 1) {
+//     console.log("Argument found");
+//   } else if (args.length > 1) {
+//     console.log("Arguments found");
+//   }
+// }
+
+// showArguments();
+// showArguments(1);
+// showArguments(2, 3, 4, 5);
+
 function showArguments(...args) {
   if (args.length < 1) {
     console.log("No argument");
@@ -8,6 +22,6 @@ function showArguments(...args) {
   }
 }
 
-showArguments();
-showArguments(1);
 showArguments(2, 3, 4, 5);
+showArguments(1);
+showArguments();
