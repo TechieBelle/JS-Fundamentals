7abe36836c96be7e1527f2d03cb0ace05d90d86e
Folashade Bello <folashadebello.b@gmail.com>
2025-05-22 17:55:35 +0100

    corrected

--- 4-concat.js
+++ 4-concat.js
@@ -1,11 +1,11 @@
 let argument = process.argv;
 
 if (argument.length < 3) {
-    console.log("undefined ")
+    console.log(undefined )
 }
  else if (argument.length == 3){
     console.log(`${argument[2]} is undefined`)
 }
 else{
-  console.log(` ${argument[2]} is ${argument[3]}`);
+  console.log(` ${argument[2]} ${argument[3]}`);
 }
