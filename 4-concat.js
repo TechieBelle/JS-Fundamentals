30df9ac1e224e73d87bd523a9d5385fa8cc19a74
Folashade Bello <folashadebello.b@gmail.com>
2025-05-22 17:29:26 +0100

    changed 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,3 +1,10 @@
 let argument = process.argv;
 
-console.log(`${argument[2]} is ${argument[3]} `);
+if (argument.length < 3) {
+    console.log("0 argument")
+}
+ else if (argument.length == 3){
+    console.log(argument[2])
+}
+else
+    console.log(`${argument[2]} is ${argument[3]} `);
