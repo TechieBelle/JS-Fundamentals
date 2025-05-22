fe7663b2e4fccca496a85faf5037ecd13e7ef2b7
Folashade Bello <folashadebello.b@gmail.com>
2025-05-22 16:44:06 +0100

    2-arguments.js corrected

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,27 +1,9 @@
-// function showArguments(...args) {
-//   if (args.length < 1) {
-//     console.log("No argument");
-//   } else if (args.length == 1) {
-//     console.log("Argument found");
-//   } else if (args.length > 1) {
-//     console.log("Arguments found");
-//   }
-// }
+const argument = process.argv.slice(2);
 
-// showArguments();
-// showArguments(1);
-// showArguments(2, 3, 4, 5);
-
-function showArguments(...args) {
-  if (args.length < 1) {
-    console.log("No argument");
-  } else if (args.length == 1) {
-    console.log("Argument found");
-  } else if (args.length > 1) {
-    console.log("Arguments found");
-  }
+if (argument.length == 0) {
+  console.log("No argument");
+}else if (argument.length == 1) {
+  console.log("Argument found");
+}else  {
+  console.log("Arguments found");
 }
-
-showArguments(2, 3, 4);
-showArguments(1);
-showArguments();
