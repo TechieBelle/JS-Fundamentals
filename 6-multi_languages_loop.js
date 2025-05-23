ce038fb1d1ad5c60ba1b9f2ac21e2a45c41ad09b
Folashade Bello <folashadebello.b@gmail.com>
2025-05-23 14:08:58 +0100

    corrected

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,9 +1,9 @@
 const args = process.argv.slice(2);
 const lines = ["C is fun", "Python is cool", "Javascript is amazing"];
 
-for (let i = 0; i < args.length; i++) {
-  lines.push(args[i]);
-}
+// for (let i = 0; i < args.length; i++) {
+//   lines.push(args[i]);
+// }
 
 for (let i = 0; i < lines.length; i++) {
   console.log(lines[i]);
