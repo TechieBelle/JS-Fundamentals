424f30b62eb2710f697b857506438917f82b7ca7
Folashade Bello <folashadebello.b@gmail.com>
2025-05-23 18:21:44 +0100

    first commit

---
+++ 8-square.js
@@ -0,0 +1,15 @@
+let argument = process.argv.slice(2)
+let x = Number(argument[0])
+let shape = "X"
+if (isNaN(x)) {
+    console.log("Missing number of occurence");
+    
+} else {
+    for (let i = 0; i < x; i++){
+
+        for (let j = 0; j < x; j++) {
+            process.stdout.write("X");
+        }
+        console.log();
+    }
+}
